6b288ab7e850b8a2596cf1a572384c14267a649c
Sathishkumar33333 <sathishsathish56435@gmail.com>
2026-02-11 03:34:52 -0800

    Add files via upload

---
+++ portfolio/int a=1.java
@@ -0,0 +1,15 @@
+int a=1
+for(int i=0;i<5;i++){
+    for(int k=0;k<=i;k++){
+    system.out.println(s:" ");
+    }
+    for(int j=0;j<a;j++)
+    {
+        system.out.println("+"+" "){
+            system.out.println();
+            a++;
+        }
+    }
+    
+
+}
